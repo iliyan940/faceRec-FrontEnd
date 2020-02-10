ec224059bcb559f715ce6b802909ca538c06cf42
iiliyan <iiliyan@vmware.com>
2020-02-10 16:10:40 +0200

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/home/dashboard/dashboard.component';
+import { MenuComponent } from './core/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
